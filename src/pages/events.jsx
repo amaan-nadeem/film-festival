f5a3fde076b6c9a5b27e403886fcd3373db7a70b
amaan-nadeem <amaannadeem@outlook.com>
2021-05-08 18:49:01 +0500

    feat: added events page

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -6,6 +6,17 @@ import SEO from "../components/seo"
 import Header from "../components/common/Header"
 import Features from "../components/Features"
 import featuresData from "../site-data/features"
+import LatestBlogs from "../components/LatestBlogs"
+import latestBlogsData from "../site-data/latest-blogs"
+import FilmsSlider from "../components/FilmsSlider"
+import SocialMedia from "../components/SocialMedia"
+import socialMediaData from "../site-data/social-media"
+import ScheduledFestivals from "../components/ScheduledFestivals"
+import Submissions from "../components/Submissions"
+import AwardCategories from "../components/AwardCategories"
+import scheduledFestivalsData from "../site-data/scheduled-festivals"
+import submissionsData from "../site-data/submissions"
+import awardCategoriesData from "../site-data/award-categories"
 
 const Events = () => {
   return (
@@ -23,6 +34,32 @@ const Events = () => {
         title={featuresData.title}
         description={featuresData.description}
         contentBoxes={featuresData.featureBoxes}
+      />
+      <ScheduledFestivals
+        isBgGray
+        title={scheduledFestivalsData.title}
+        festivals={scheduledFestivalsData.festivals}
+      />
+      <Submissions
+        title={submissionsData.title}
+        contentBoxes={submissionsData.featureBoxes}
+      />
+      <AwardCategories
+        isBgGray
+        title={awardCategoriesData.title}
+        percentageBoxes={awardCategoriesData.percentageBoxes}
+      />
+      <FilmsSlider films={[{}, {}, {}, {}]} />
+      <LatestBlogs
+        isBgGray
+        title={latestBlogsData.title}
+        description={latestBlogsData.description}
+        linkText={latestBlogsData.linkText}
+        blogs={latestBlogsData.blogs}
+      />
+      <SocialMedia
+        title={socialMediaData.title}
+        mediaPlatforms={socialMediaData.mediaPlatforms}
       />
     </Layout>
   )
